434af6a7b985af372f02b6f89dbf4966a0ed8d0a
Ash2803 <ashot.93@mail.ru>
2023-01-10 13:50:36 +0400

    Split books by two columns.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import json
-from livereload import Server
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+from more_itertools import chunked
 
 
 def on_reload(template):
     with open("books.json", "r") as my_file:
         books_json = my_file.read()
     books = json.loads(books_json)
+    chunked_books = list(chunked(books, 2))
     rendered_page = template.render(
-        books=books
+        chunked_books=chunked_books
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
